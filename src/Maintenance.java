08fce2d8060a511dc116f3c33417e10f34d52bd7
ConwayJ18 <conway.jess18@gmail.com>
2019-02-18 19:42:40 -0600

    Rough draft of Maintenance

--- src/Maintenance.java
+++ src/Maintenance.java
@@ -3,28 +3,35 @@ public class Maintenance extends Facility
 	private String details;
 	private int cost;
 	
+	//Default constructor
 	public Maintenance()
 	{
-		
 	}
 	
-	public Object getDetails()
+	//Full constructor
+	public Maintenance(String details, int cost)
 	{
-
+		this.details = details;
+		this.cost = cost;
 	}
 	
-	public Object setDetails()
+	public String getDetails(String details)
 	{
-
+		return details;
 	}
 	
-	public Object getCost()
+	public void setDetails()
 	{
-
+		this.details = details;
 	}
 	
-	public Object setCost() 
+	public int getCost()
 	{
-
+		return cost;
+	}
+	
+	public void setCost(int cost) 
+	{
+		this.cost = cost;
 	}
 }
