d4f8218c02012ff7b4f587b1c544c68e2615b7c7
ConwayJ18 <conway.jess18@gmail.com>
2019-02-22 21:05:46 -0600

    Attempted to fill in parameters of FacilityDriver methods

---
+++ src/comp473s19project1/FacilityDAL.java
@@ -0,0 +1,34 @@
+package comp473s19project1;
+
+public class FacilityDAL
+{
+	public Object listFacilities()
+	{
+		return null;
+	}
+	
+	public Object getFacilityInformation(Facility f)
+	{
+		return null;
+	}
+	
+	public Object requestAvailableCapacity(Facility f)
+	{
+		return null;
+	}
+	
+	public Object addNewFacility(Facility f)
+	{
+		return null;
+	}
+	
+	public void addFacilityDetail(Facility f, FacilityDetail fd)
+	{
+		
+	}
+	
+	public Object removeFacility(Facility f)
+	{
+		return null;
+	}
+}
