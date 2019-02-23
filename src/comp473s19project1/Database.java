b288c01d26ee15d9a8a552a0c6551eef894f4362
ConwayJ18 <conway.jess18@gmail.com>
2019-02-22 21:13:00 -0600

    Added database to help hold & reference specific Facilities

---
+++ src/comp473s19project1/Database.java
@@ -0,0 +1,13 @@
+package comp473s19project1;
+
+import java.util.HashMap;
+
+public class Database
+{
+	static HashMap<Facility, Facility> FacilitiesList;
+	
+	public Database()
+	{
+		FacilitiesList = new HashMap<Facility, Facility>();
+	}
+}
