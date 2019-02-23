41f2cceab40036493cd8176b4a1499dc4810c4f6
ConwayJ18 <conway.jess18@gmail.com>
2019-02-23 16:09:07 -0600

    Added all necessary parts for testing

--- src/comp473s19project1/FacilityDriver.java
+++ src/comp473s19project1/FacilityDriver.java
@@ -1,39 +1,87 @@
 package comp473s19project1;
 
+import java.util.ArrayList;
+
 public class FacilityDriver
 {
 	public static void main(String args[])
 	{
-		//Begin creation of test objects
-		FacilityDAL fdal = new FacilityDAL();
+		/*
+		 * The following objects are needed in order to begin testing
+		 */
 		
+		//Begin creation of primary test objects
 		FacilityLocation fl1 = new FacilityLocation(1032, "W", "Sheridan Rd", "Chicago", "IL", 60626, "United States");
 		FacilityDetail fd1 = new FacilityDetail(1, "Information Commons", 5574, 67000);
 		
+		UseSchedule us1 = new UseSchedule();
+		FacilityUse u1 = new FacilityUse(us1);
+		
+		MaintenanceSchedule ms1 = new MaintenanceSchedule();
+		MaintenanceLog ml1 = new MaintenanceLog();
+		FacilityMaintenance m1 = new FacilityMaintenance(ms1, ml1);
+		Facility f1 = new Building(fl1, fd1, u1, m1);
+		
+		//Begin creation of secondary test objects
 		Time start1 = new Time(1, 0);
 		Time end1 = new Time(2, 0);
 		Days d1 = new Days(true,false,true,false,true,false,false);
 		Slot s1 = new Slot(d1, start1, end1);
 		FacilityUser fu1 = new FacilityUser("jaconway", "Jess Conway", 7864938008l, "jconway6@luc.edu", "Admin");
-		UseSchedule us1 = new UseSchedule();
-		FacilityUse u1 = new FacilityUse(fu1, us1);
+		UseRequest ur1 = new UseRequest(fu1, s1);
 		
-		Time start2 = new Time(7, 15);
-		Time end2 = new Time(9, 20);
-		Date d2 = new Date(3, 30, 2019);
+		Time start2 = new Time(15, 29);
+		Time end2 = new Time(19, 17);
+		Date d2 = new Date(12,21,2019);
 		Slot s2 = new Slot(d2, start2, end2);
-		MaintenanceSchedule ms1 = new MaintenanceSchedule();
-		MaintenanceOrder mo1 = new MaintenanceOrder();
-		MaintenanceLog ml1 = new MaintenanceLog();
-		MaintenanceRequest mr1 = new MaintenanceRequest();
-		FacilityMaintenance m1 = new FacilityMaintenance(ms1, mo1, ml1, mr1);
+		Inspection i1 = new Inspection("Fire", s2);
+
+		Date d3 = new Date(3, 30, 2019);
+		MaintenanceRequest mr1 = new MaintenanceRequest(5, "Flooding on 1st Floor", d3);
+		Date d4 = new Date(4,5,2019);
+		Time start4 = new Time(14, 00);
+		Duration duration4 = new Duration(120);
+		Slot s4 = new Slot(d4, start4, duration4);
+		MaintenanceOrder mo1 = new MaintenanceOrder(mr1, d4, 14100);
+		//End creation of secondary test objects
 		
-		Time start3 = new Time(15, 29);
-		Time end3 = new Time(19, 17);
-		Date d3 = new Date(12,21,2019);
-		Slot s3 = new Slot(d3, start3, end3);
-		Inspection i1 = new Inspection("Fire", s3);
-		Facility f1 = new Building(fl1, fd1, u1, m1, i1);
-		//End creation of test objects
+		
+		
+		/*
+		 * The following code is used in place of JUnit tests to test the DAL
+		 */
+		
+		//Begin FacilityDAL tests
+		FacilityDAL fClient = new FacilityDAL();
+		fClient.addNewFacility(f1);
+		fClient.addFacilityDetail(f1, fd1);
+		fClient.listFacilities();
+		fClient.requestAvailableCapacity(f1);
+		fClient.removeFacility(f1);
+		//End FacilityDAL tests
+		
+		//Begin UseDAL tests
+		f1.addInspection(i1);
+		fClient.addNewFacility(f1);
+		UseDAL uClient = new UseDAL();
+		uClient.isInUseDuringInterval(f1, d2, start2, end2);
+		uClient.assignFacilityToUse(f1, ur1);
+		uClient.vacateFacility(f1);
+		uClient.listInspections(f1);
+		uClient.listActualUsage(f1);
+		uClient.calcUsageRate(f1);
+		//End UseDAL tests
+		
+		//Begin MaintenanceDAL tests
+		MaintenanceDAL mClient = new MaintenanceDAL();
+		mClient.makeFacilityMaintRequest(f1, mr1);
+		mClient.scheduleMaintenance(f1, mo1, s4);
+		mClient.calcMaintenanceCostForFacility(f1);
+		mClient.calcProblemRateForFacility(f1);
+		mClient.calcDownTimeForFacility(f1);
+		mClient.listMaintRequests(f1);
+		mClient.listMaintenance(f1);
+		mClient.listFacilityProblems(f1);
+		//End MaintenanceDAL tests
 	}
 }
