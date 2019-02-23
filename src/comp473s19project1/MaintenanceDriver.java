0f0a61e45358eea099ad0658fa2a1e814c20f0af
ConwayJ18 <conway.jess18@gmail.com>
2019-02-23 16:22:22 -0600

    Deleted and renamed Driver files. Now there is only one

--- src/comp473s19project1/MaintenanceDriver.java
+++
@@ -1,9 +0,0 @@
-package comp473s19project1;
-
-public class MaintenanceDriver
-{
-	public static void main(String args[])
-	{
-		
-	}
-}
--- src/comp473s19project1/UseDriver.java
+++
@@ -1,9 +0,0 @@
-package comp473s19project1;
-
-public class UseDriver
-{
-	public static void main(String args[])
-	{
-		
-	}
-}
